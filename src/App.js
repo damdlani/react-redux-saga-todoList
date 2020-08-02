f06d3a80db9ac9269545972bad59547a416d6b6d
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-08-02 18:36:40 +0200

    changed default reactapp files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from './Form'
+import Tasks from './Tasks'
+import Buttons from './Buttons'
+
+const tasks = [
+  {id: 1, content: "test31", done: false },
+  {id: 2, content: "test2", done: false },
+];
+
+const hideDoneTasks = false;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <header className="header">
+            <h1 className="header__title">Lista zadań</h1>
+        </header>
+        <main className="todo">
+            <section className="todo__new">
+                <h2 className="todo__title">Dodaj nowe zadanie</h2>
+                <Form />
+            </section>
+            <section className="todo__list">
+                <div className="todo__header">
+                    <h2 className="todo__title">Lista zadań</h2>                  
+                    <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
+                </div>
+                <div className="todo__tasks">
+                    <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
+                </div>
+            </section>
+        </main>
+        <footer className="footer">&copy;Krzysztof Kwieciński 2020</footer>
     </div>
   );
 }
