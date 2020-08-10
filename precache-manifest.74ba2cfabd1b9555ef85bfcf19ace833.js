f26ab1088b0ced6c1293fccd1bb1002a64f100f4
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-08-10 08:34:38 +0200

    Updates

---
+++ precache-manifest.74ba2cfabd1b9555ef85bfcf19ace833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12bcd420b83d7375bb19d2cfa470ddac",
+    "url": "/react-todoList/index.html"
+  },
+  {
+    "revision": "de5fd047524728730594",
+    "url": "/react-todoList/static/css/main.ed6c871d.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/react-todoList/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todoList/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de5fd047524728730594",
+    "url": "/react-todoList/static/js/main.8f4670db.chunk.js"
+  },
+  {
+    "revision": "368cc5ef62247b352046",
+    "url": "/react-todoList/static/js/runtime-main.9cfac244.js"
+  }
+]);
