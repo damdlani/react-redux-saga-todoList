8b1c1258753d9b4e6c61d1fc54e6ba31a5279650
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-08-17 08:23:56 +0200

    replaced few exact elements with one variable

--- src/Form/index.js
+++ src/Form/index.js
@@ -6,8 +6,11 @@ const Form = ({ addNewTask }) => {
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-        if (newTaskContent === ""){return};
-        addNewTask(newTaskContent.trim());
+        const trimmedTaskContent = newTaskContent.trim();
+        if (trimmedTaskContent === ""){
+            return
+        };
+        addNewTask(trimmedTaskContent);
         setNewTaskContent("");
     };
     const changeNewTaskContent = (event) => {
