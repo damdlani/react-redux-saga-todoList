9ba3d9dd2bc7985ae28198028944468566e29a9c
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-11-16 09:04:27 +0100

    prevent leaving untitled task

--- src/features/tasks/SingleTaskPage/index.js
+++ src/features/tasks/SingleTaskPage/index.js
@@ -1,71 +1,101 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import Container from '../../../common/Container';
-import Footer from '../../../common/Footer';
-import { StyledSection } from '../../../common/Section/styled';
-import { changeTaskDetail, changeTaskContent, selectTaskByID, toggleTaskDone } from '../TasksPage/tasksSlice';
-import { EditButton, DoneButton, TaskContentForm, TaskSpan, TaskDetail, TaskHeader, DetailForm, TextArea, ContentInput } from './styled';
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Container from "../../../common/Container";
+import Footer from "../../../common/Footer";
+import { StyledSection } from "../../../common/Section/styled";
+import {
+  changeTaskDetail,
+  changeTaskContent,
+  selectTaskByID,
+  toggleTaskDone,
+} from "../TasksPage/tasksSlice";
+import {
+  EditButton,
+  DoneButton,
+  TaskContentForm,
+  TaskSpan,
+  TaskDetail,
+  TaskHeader,
+  DetailForm,
+  TextArea,
+  ContentInput,
+} from "./styled";
 
 export const SingleTaskPage = () => {
   const { id } = useParams();
-  const { content, done, date, detail} = useSelector(state => selectTaskByID(state, id));
+  const { content, done, date, detail } = useSelector((state) =>
+    selectTaskByID(state, id)
+  );
   const [taskDetail, setTaskDetail] = useState(detail);
   const [taskContent, setTaskContent] = useState(content);
   const textarea = useRef(null);
   const input = useRef(null);
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
-    dispatch(changeTaskDetail({ id, detailContent: taskDetail }))
-    dispatch(changeTaskContent({ id, taskContent: taskContent }))
-  }, [taskDetail, taskContent])
-  
+    dispatch(changeTaskDetail({ id, detailContent: taskDetail }));
+    dispatch(changeTaskContent({ id, taskContent: taskContent }));
+  }, [taskDetail, taskContent]);
+
   const onFormSubmit = (event) => {
     event.preventDefault();
-    document.activeElement.blur() 
-  }
+    if(!taskContent){
+      setTaskContent("Zadanie bez nazwy")
+    };
+    document.activeElement.blur();
+  };
 
   const changeDetailContent = (event) => {
-    setTaskDetail(event.target.value)
-  }
+    setTaskDetail(event.target.value);
+  };
   const changeTaskTitleContent = (event) => {
-    setTaskContent(event.target.value)
-  }
+    const trimmedTask = event.target.value.trim();
+    setTaskContent(trimmedTask);
+  };
   return (
-  <Container>
+    <Container>
       <StyledSection>
         <TaskHeader>
-        
           <TaskContentForm onSubmit={onFormSubmit}>
-            <EditButton type="button" onClick={() => input.current.focus()}><i class="fas fa-pencil-alt"></i></EditButton>
-            <ContentInput 
+            <EditButton type="button" onClick={() => input.current.focus()}>
+              <i class="fas fa-pencil-alt"></i>
+            </EditButton>
+            <ContentInput
               value={taskContent}
               type="text"
-              ref={input} 
+              ref={input}
               onChange={changeTaskTitleContent}
             />
           </TaskContentForm>
-            
-          <TaskSpan>Dodano: {date}</TaskSpan> 
+
+          <TaskSpan>Dodano: {date}</TaskSpan>
         </TaskHeader>
         <TaskDetail>
           <TaskSpan>Szczegóły zadania: </TaskSpan>
-          <EditButton onClick={() => textarea.current.focus()}><i class="fas fa-pencil-alt"></i></EditButton>
-          <DetailForm onSubmit={onFormSubmit} >
-            <TextArea  
-              ref={textarea} rows={6} 
-              placeholder={'Nie dodano szczegółów.'} 
-              value={taskDetail} 
+          <EditButton onClick={() => textarea.current.focus()}>
+            <i class="fas fa-pencil-alt"></i>
+          </EditButton>
+          <DetailForm onSubmit={onFormSubmit}>
+            <TextArea
+              ref={textarea}
+              rows={6}
+              placeholder={"Nie dodano szczegółów."}
+              value={taskDetail}
               onChange={changeDetailContent}
               type="text"
             />
           </DetailForm>
 
-          <TaskSpan>Ukończone: <DoneButton onClick={() => dispatch(toggleTaskDone(id))}>{done ? "tak" : "nie"}</DoneButton> </TaskSpan>
+          <TaskSpan>
+            Ukończone:{" "}
+            <DoneButton onClick={() => dispatch(toggleTaskDone(id))}>
+              {done ? "tak" : "nie"}
+            </DoneButton>{" "}
+          </TaskSpan>
         </TaskDetail>
       </StyledSection>
       <Footer content="&copy;Krzysztof Kwieciński 2020" />
-  </Container>
-  )
+    </Container>
+  );
 };
